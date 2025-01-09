4e70a955e111642fca54688b5a764568a054fc46
ihletpl <ihletpl@gmail.com>
2025-01-09 01:11:31 +0000

    idx

---
+++ .idx/dev.nix
@@ -0,0 +1,52 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.hugo
+  ];
+
+  # Sets environment variables in the workspace
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [
+            "hugo"
+            "server"
+            "-D"
+            "-F"
+            "--bind" "0.0.0.0"
+            "--port" "$PORT"
+            "--baseURL" "$PREVIEW_URL"
+          ];
+          manager = "web";
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
